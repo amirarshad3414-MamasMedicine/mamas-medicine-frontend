5bf3c06906e8e08400c1b23e4931eeb3808e99b6
Umairali0987 <umairali8245@gmail.com>
2026-04-16 15:16:59 +0500

    I have removed the historical import

--- app/subscribe-marketing/route.js
+++ app/subscribe-marketing/route.js
@@ -34,7 +34,6 @@ export async function POST(req) {
                 },
               ],
             },
-            historical_import: false,
           },
           relationships: {
             list: {
